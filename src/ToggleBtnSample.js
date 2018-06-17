81bc0fc26210de10c97a306a3ae041ee5bc9900f
shinshin86 <beagles1986@gmail.com>
2018-06-17 15:50:47 +0900

    Add toggle btn 2

--- src/ToggleBtnSample.js
+++ src/ToggleBtnSample.js
@@ -3,7 +3,12 @@ import styled from 'styled-components';
 
 const Container = styled.div`
   margin: 30px;
-  width: 100%;
+  padding: 30px;
+  border: solid 1px #ccc;
+`;
+
+const ExBtnContainer = styled.div`
+  margin: 100px;
   border: solid 1px #ccc;
 `;
 
@@ -17,6 +22,17 @@ const ToggleBtn = styled.button`
     show &&
     `
     background: #DE5065;
+  `};
+`;
+
+const ExToggleBtn = styled(ToggleBtn)`
+  color: #de5;
+  width: 250px;
+  height: 70px;
+  ${({ show }) =>
+    show &&
+    `
+    background: #89e;
   `};
 `;
 
@@ -39,6 +55,11 @@ export default class ToggleBtnSample extends Component {
         <ToggleBtn show={this.state.isShow} onClick={this.handleToggleBtn}>
           Toggle Button
         </ToggleBtn>
+        <ExBtnContainer>
+          <ExToggleBtn show={this.state.isShow} onClick={this.handleToggleBtn}>
+            Toggle Button
+          </ExToggleBtn>
+        </ExBtnContainer>
       </Container>
     );
   }
